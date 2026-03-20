00225d36028ee126ae69954c2b3eb68bbec7614b
Aaron <hatomemes@gmail.com>
2026-03-20 04:17:09 -0700

    implement front end pages

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,12 +1,28 @@
 class CalculationsController < ApplicationController
+
   def new
     @calculation = Calculation.new
   end
 
   def create
+    @calculation = Calculation.new(calculation_params)
+
+    @calculation.solution = "Pending"
+    @calculation.tree = { status: "Not parsed" }
+
+    if @calculation.save
+      redirect_to calculation_path(@calculation)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
     @calculation = Calculation.find(params[:id])
   end
+
+  def calculation_params
+    params.require(:calculation).permit(:equation)
+  end
+  
 end
